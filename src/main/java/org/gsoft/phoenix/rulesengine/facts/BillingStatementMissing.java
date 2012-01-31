7dd5cf6c9db6a5df11b6e9a55bcbcc1f9000bdec
Kevin Gaasch <gaaschk@gmail.com>
2012-01-31 19:42:58 +0000

    Rule was failing for loans added with repayments start dates in the future.

--- src/main/java/org/gsoft/phoenix/rulesengine/facts/BillingStatementMissing.java
+++ src/main/java/org/gsoft/phoenix/rulesengine/facts/BillingStatementMissing.java
@@ -1,10 +1,14 @@
 package org.gsoft.phoenix.rulesengine.facts;
 
+import org.gsoft.phoenix.buslogic.system.SystemSettingsLogic;
 import org.gsoft.phoenix.domain.loan.Loan;
+import org.gsoft.phoenix.domain.loan.LoanTypeProfile;
 import org.gsoft.phoenix.domain.payment.BillingStatement;
+import org.gsoft.phoenix.repositories.loan.LoanTypeProfileRepository;
 import org.gsoft.phoenix.repositories.payment.BillingStatementRepository;
 import org.gsoft.phoenix.rulesengine.AbstractFact;
 import org.gsoft.phoenix.util.ApplicationContextLocator;
+import org.joda.time.DateTime;
 import org.springframework.context.ApplicationContext;
 
 public class BillingStatementMissing extends AbstractFact{
@@ -37,6 +41,10 @@ public class BillingStatementMissing extends AbstractFact{
 		ApplicationContext applicationContext = ApplicationContextLocator.getApplicationContext();
 		BillingStatementRepository billingStatementRepository = (BillingStatementRepository)applicationContext.getBean(BillingStatementRepository.class);
 		BillingStatement lastStatement = billingStatementRepository.findMostRecentBillingStatementForLoan(this.getLoan().getLoanID());
-		return (lastStatement == null || lastStatement.getDueDate().before(this.getLoan().getNextDueDate()));
+		SystemSettingsLogic systemSettings = applicationContext.getBean(SystemSettingsLogic.class);
+		LoanTypeProfile ltp = applicationContext.getBean(LoanTypeProfileRepository.class).findOne(loan.getEffectiveLoanTypeProfileID());
+		DateTime dueDateMinusWindow = new DateTime(loan.getCurrentUnpaidDueDate()).minusDays(ltp.getDaysBeforeDueToBill());
+		DateTime sysDate = new DateTime(systemSettings.getCurrentSystemDate());
+		return (!dueDateMinusWindow.isAfter(sysDate) && (lastStatement == null || lastStatement.getDueDate().before(this.getLoan().getNextDueDate())));
 	}
 }
